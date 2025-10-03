4b2ec39563b3f92bab19d1c48d2aa5b7d30badf9
Laureano Galan <lauregalan1@gmail.com>
2025-10-03 18:46:55 -0300

    Add code to abstractDAO

---
+++ demo/src/main/java/com/logistic/reeasy/demo/scan/abs/AbstractDAO.java
@@ -0,0 +1,43 @@
+package com.logistic.reeasy.demo.scan.abs;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import org.sql2o.Sql2o;
+
+public abstract class AbstractDAO {
+
+    private final Sql2o sql2o;
+
+    public AbstractDAO(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    /* usando reflexion para la bd */
+    public <T> void insert(T entity, String tableName) {
+        Class<?> clazz = entity.getClass();
+        Field[] fields = clazz.getDeclaredFields();
+
+        // Generar columnas y placeholders dinámicos
+        String columns = Arrays.stream(fields)
+                .map(Field::getName)
+                .collect(Collectors.joining(", "));
+        String values = Arrays.stream(fields)
+                .map(f -> ":" + f.getName())
+                .collect(Collectors.joining(", "));
+
+        String sql = "INSERT INTO " + tableName + " (" + columns + ") VALUES (" + values + ")";
+
+        try (var con = sql2o.open()) {
+            var query = con.createQuery(sql);
+            for (Field field : fields) {
+                field.setAccessible(true); // para acceder a campos privados
+                query.addParameter(field.getName(), field.get(entity));
+            }
+            query.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
