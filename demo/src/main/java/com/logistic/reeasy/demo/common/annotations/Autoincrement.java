1d1f6cb392a8b1a56f228647555eb425388d8c51
Lautaro Asat <lautaronorielasat@gmail.com>
2025-11-05 17:13:20 -0300

    Fix Insert and return the real entity inserted

---
+++ demo/src/main/java/com/logistic/reeasy/demo/common/annotations/Autoincrement.java
@@ -0,0 +1,9 @@
+package com.logistic.reeasy.demo.common.annotations;
+
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Autoincrement {
+}
