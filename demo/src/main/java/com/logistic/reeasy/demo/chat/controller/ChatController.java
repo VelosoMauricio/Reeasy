496d457e1447d0f0fc1a0309c77b9cc9b0d7b186
Lautaro Asat <lautaronorielasat@gmail.com>
2025-11-07 21:28:52 -0300

    Improve chatbot

--- demo/src/main/java/com/logistic/reeasy/demo/chat/controller/ChatController.java
+++ demo/src/main/java/com/logistic/reeasy/demo/chat/controller/ChatController.java
@@ -3,50 +3,66 @@ package com.logistic.reeasy.demo.chat.controller;
 import com.logistic.reeasy.demo.chat.service.ChatService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
-import org.springframework.ai.embedding.EmbeddingResponse;
-import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Controller
 @Slf4j
 public class ChatController {
 
     private ChatService chatService;
-    private ChatClient chatClient;
+    private ChatClient.Builder chatClientBuilder;
+    private ChatModel chatModel;
 
-    public ChatController(ChatService chatService, ChatClient chatClient){
+    public ChatController(ChatService chatService, ChatClient.Builder chatClientBuilder, ChatModel chatModel) {
         this.chatService = chatService;
-        this.chatClient = chatClient;
+        this.chatClientBuilder = chatClientBuilder;
+        this.chatModel = chatModel;
     }
 
     @GetMapping("/ask")
     @ResponseBody
     public Map<String, String> ask(@RequestParam("q") String question) {
 
+        log.info("Generating vector store from data source...");
         VectorStore vectorStore = chatService.loadDataInVectorStore();
+        log.info("Vector store generation completed.");
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
+        ChatClient chatClient = chatClientBuilder
+            .defaultAdvisors(
+                    MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                    QuestionAnswerAdvisor.builder(vectorStore).build()
+            )
+            .defaultOptions(ChatOptions.builder().temperature(0.7).build())
+            .build();
+
 
         log.info("Start responding to user question: {}", question);
 
         var response = chatClient
                 .prompt("You are a helpful assistant that helps users find information about coupons based on the provided context.")
-                .advisors(new QuestionAnswerAdvisor(vectorStore))
                 .user(question)
                 .call();
 
         log.info("Finish responding to user question.");
 
-        System.out.println("Respuesta del modelo: " + response.content());
-
         String answer = response.content();
 
         return Map.of("response", answer);
+
     }
 }
--- demo/src/main/java/com/logistic/reeasy/demo/chat/di/AiConfig.java
+++ demo/src/main/java/com/logistic/reeasy/demo/chat/di/AiConfig.java
@@ -1,5 +1,6 @@
 package com.logistic.reeasy.demo.chat.di;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.embedding.EmbeddingModel;
@@ -19,12 +20,11 @@ public class AiConfig {
         return SimpleVectorStore.builder(embeddingModel).build();
     }
 
-
     @Bean
-    ChatClient chatClient(ChatModel chatModel) { // <-- Inject the MODEL
+    public ChatClient.Builder chatClientBuilder(ChatModel chatModel) {
         return ChatClient.builder(chatModel)
-                .defaultAdvisors()
-                .defaultOptions(ChatOptions.builder().topK(100).temperature(.7).build())
-                .build();
+                .defaultOptions(ChatOptions.builder().topK(100).temperature(.7).build());
     }
+
+
 }
